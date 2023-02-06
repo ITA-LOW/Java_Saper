f33c246d9e592f5b032c5ffaf170b4e8941ec5ba
ITA <italo.mtm@gmail.com>
2023-02-06 20:03:03 -0300

    organizando em pastas

--- aula3/App.java
+++ aula3/App.java
@@ -1,10 +1,4 @@
-import aula3.exer4;
-import aula3.exer5;
-import aula3.exer6;
-import aula3.exerDois;
-import aula3.exerTres;
-import aula3.exerUm;
-
+package aula3;
 public class App {
     public static void main(String[] args) throws Exception {
         //exerUm aluno_jao = new exerUm();
