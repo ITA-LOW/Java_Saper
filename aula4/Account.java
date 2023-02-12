a13751e28129b4154e78584c197e3219499e0587
ITA <italo.mtm@gmail.com>
2023-02-12 15:27:16 -0300

    aula 4

---
+++ aula4/Account.java
@@ -0,0 +1,45 @@
+package aula4;
+
+public class Account {
+    private String nome;
+    private Double saldoBancario;
+    public Account(String nome){
+        this.nome = nome;
+    }
+    
+
+    public void setName(String nome){//void -> tipo de retorno do método, não retorna nada
+        this.nome = nome;            // na próxima função fica mais claro   
+    }
+
+    public String getName() {
+        return nome;
+    }
+
+    public void setSaldo(double saldo){
+        if(saldo >= 0){
+        saldoBancario = saldo;
+        } else{
+            saldoBancario = 0.0;
+        }
+    }
+
+    public double getSaldo(){
+        return saldoBancario;
+    }
+
+    public void setDeposito(double deposito){
+        if(deposito > 0 ){
+            saldoBancario = saldoBancario + deposito;
+        } else {
+            System.out.println("Valor de depósito inválido");
+        }
+        
+    }
+}
+
+
+
+    
+
+
---
+++ aula4/App.java
@@ -0,0 +1,58 @@
+package aula4;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception{
+        //============
+        /*
+        Account minhaConta = new Account("italo");
+        //System.out.printf("Nome da conta: %s%n", minhaConta.getName());
+        //System.out.print("Entre com o nome do cliente: ");
+        Scanner input = new Scanner(System.in);
+        minhaConta.setName(input.nextLine());
+        //System.out.printf("Nome da conta 1: %s%n", minhaConta.getName());
+        minhaConta.setSaldo(1200.56);
+        //System.out.printf("Saldo da conta 1: %s%n", minhaConta.getName());
+        
+        
+        //============== 
+        //Account minhaConta2 = minhaConta;// minhaconta2 ta recebendo minhaconta
+        //System.out.printf("Nome da conta: %s%n", minhaConta2.getName());
+        //Quando inicia uma variável como objeto essa variável aponta para
+        //o mesmo endereço de memória que está o objeto.
+        //==============
+        
+        //==============
+        Account minhaConta2 = new Account("py");
+        minhaConta2.setName(minhaConta.getName());
+        minhaConta2.setSaldo(minhaConta.getSaldo());
+
+        System.out.printf("Nome da conta 2: %s%n", minhaConta2.getName());
+        System.out.printf("Saldo da conta 2: %s%n", minhaConta2.getSaldo());
+        //==============
+
+        minhaConta.setName("Python");
+        minhaConta.setSaldo(minhaConta.getSaldo()*1.2);
+        System.out.println("==========================");
+
+        System.out.printf("Nome da conta 1: %s%n", minhaConta.getName());
+        System.out.printf("Saldo da conta 1: %s%n", minhaConta.getSaldo());
+        System.out.println("==========================");*/
+        
+        /*
+        Account minhaConta = new Account("italo"); //um construtor que já passa um parâmetro
+        minhaConta.setSaldo(1200.56);
+        System.out.printf("Nome da conta 1: %s%n", minhaConta.getName());
+        System.out.printf("Saldo da conta 1: %.2f", minhaConta.getSaldo());
+         */
+
+        Account Conta1 = new Account("italo");
+        Conta1.setSaldo(-10.0);
+        Conta1.getSaldo();
+        Conta1.setDeposito(15.75);
+
+        System.out.printf("nome e saldo : %s -> %.2f", Conta1.getName(), Conta1.getSaldo()); 
+
+    }
+
+}
